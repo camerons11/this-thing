4ace2e140bb261aaed56baf7c14ee2d43d5eee13
camerons11 <shaffercm@g.crookcountyschools.org>
2021-12-01 01:55:49 -0800

    2am

---
+++ script.js
@@ -0,0 +1,15 @@
+onsole.log('my script is working')
+
+document.write("<h1>i am doing this at 1am</h1>");
+
+alert('would you mind answering this question');
+alert('hey i just really need to ask you something');
+
+prompt('are you alive')
+
+let answer = prompt('are you alive?')
+if (answer === "yes"){
+EPIC}
+
+if (answer === "no") SUCKS
+}
