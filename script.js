2aa5627c72e4e3168ad7197b1d323d0c7acd7084
camerons11 <shaffercm@g.crookcountyschools.org>
2021-12-01 03:16:09 -0800

    im tired

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-onsole.log('my script is working')
-
+console.log('my script is working')
+console.log('i am doing this really late')
 document.write("<h1>i am doing this at 1am</h1>");
+const num1 = 5;
+const num2 = 3;
+
+// add two numbers
+const sum = num1 + num2;
+
+// display the sum
+console.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum);
+const num1 = parseInt(prompt('Enter the first number '));
+const num2 = parseInt(prompt('Enter the second number '));
+const sum = num1 + num2;
+console.log(`The sum of ${num1} and ${num2} is ${sum}`);
 
 alert('would you mind answering this question');
 alert('hey i just really need to ask you something');
@@ -11,5 +23,5 @@ let answer = prompt('are you alive?')
 if (answer === "yes"){
 EPIC}
 
-if (answer === "no") SUCKS
+if (answer === "no") { SUCKS
 }
